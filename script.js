51067f92fa12843350f188b810ea97673cbfea36
FoxyBryn <155707565+FoxyBryn@users.noreply.github.com>
2024-01-23 12:55:36 +1100

    Add getComputerChoice function.

---
+++ script.js
@@ -0,0 +1,28 @@
+function getComputerChoice() {
+    const options = ["Rock","Paper","Scissors"]
+    const choice = options[Math.floor(Math.random()*options.length)]
+    return choice;
+}
+
+console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
