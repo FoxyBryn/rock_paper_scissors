48574a559922ed5d276fd2f4c1d2bfc928198d0f
FoxyBryn <155707565+FoxyBryn@users.noreply.github.com>
2024-01-23 14:21:46 +1100

    Add score variables.

--- script.js
+++ script.js
@@ -1,44 +1,89 @@
 function getComputerChoice() {
-    const options = ["ROCK","PAPER","SCISSORS"]
-    const choice = options[Math.floor(Math.random()*options.length)]
+    let options = ["ROCK","PAPER","SCISSORS"]
+    let choice = options[Math.floor(Math.random()*options.length)]
     return choice;
 }
 
-console.log(getComputerChoice())
+let playerScore = 0;
+let computerScore = 0;
+
+let playerSelection = prompt("Scissors, Paper, Rock?");
+let computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection.toUpperCase() === computerSelection) {
-        return "Tie";
-    } else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "PAPER") {
-        return "You lose! Paper beats Rock!";
-    } else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "SCISSORS") {
-        return "You win! Rock beats Scissors!";
-    } else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "SCISSORS") {
-        return "You lose! Scissors beats Paper!";
-    } else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "ROCK") {
-        return "You win! Paper beats Rock!";
-    } else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "ROCK") {
-        return "You lose! Rock beats Scissors!";
-    } else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "PAPER") {
-        return "You win! Scissors beats Paper!";
+        let result = "A tie! Let's play again!"
+        result += "\n"
+        result += "Your Score: " + playerScore
+        result += "\n"
+        result += "Computer Score: " + computerScore
+        return result;
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "PAPER") {
+        computerScore++;
+        let result = "You lose! Paper beats Rock!"
+        result += "\n"
+        result += "Your Score: " + playerScore
+        result += "\n"
+        result += "Computer Score: " + computerScore
+        return result;
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "SCISSORS") {
+        playerScore++;
+        let result = "You win! Rock beats Scissors!"
+        result += "\n"
+        result += "Your Score: " + playerScore
+        result += "\n"
+        result += "Computer Score: " + computerScore
+        return result;
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "SCISSORS") {
+        computerScore++;
+        let result = "You lose! Scissors beats Paper!"
+        result += "\n"
+        result += "Your Score: " + playerScore
+        result += "\n"
+        result += "Computer Score: " + computerScore
+        return result;
+    } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "ROCK") {
+        playerScore++;
+        let result = "You win! Paper beats Rock!"
+        result += "\n"
+        result += "Your Score: " + playerScore
+        result += "\n"
+        result += "Computer Score: " + computerScore
+        return result;
+    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "ROCK") {
+        computerScore++;
+        let result = "You lose! Rock beats Scissors!"
+        result += "\n"
+        result += "Your Score: " + playerScore
+        result += "\n"
+        result += "Computer Score: " + computerScore
+        return result;
+    } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection === "PAPER") {
+        playerScore++;
+        let result = "You win! Scissors beats Paper!"
+        result += "\n"
+        result += "Your Score: " + playerScore
+        result += "\n"
+        result += "Computer Score: " + computerScore
+        return result;
     } else {
-        return "nothing"
+        let result = "Please enter scissors, paper, or rock."
+        result += "\n"
+        result += "Your Score: " + playerScore
+        result += "\n"
+        result += "Computer Score: " + computerScore
+        return result;
     }
 
-
-
-
-
-
-
-
-
-
-    
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+
+console.log(computerSelection)
+
+function game () {
+    console.log(playRound(playerSelection, computerSelection))
+}
+
+game();
