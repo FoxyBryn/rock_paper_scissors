4465de7de9363a7f294e3253120738fd3ce98471
FoxyBryn <155707565+FoxyBryn@users.noreply.github.com>
2024-01-23 13:29:46 +1100

    Add playRound function.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    const options = ["Rock","Paper","Scissors"]
+    const options = ["ROCK","PAPER","SCISSORS"]
     const choice = options[Math.floor(Math.random()*options.length)]
     return choice;
 }
@@ -8,6 +8,23 @@ console.log(getComputerChoice())
 
 function playRound(playerSelection, computerSelection) {
 
+    if (playerSelection.toUpperCase() === computerSelection) {
+        return "Tie";
+    } else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "PAPER") {
+        return "You lose! Paper beats Rock!";
+    } else if (playerSelection.toUpperCase() == "ROCK" && computerSelection == "SCISSORS") {
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "SCISSORS") {
+        return "You lose! Scissors beats Paper!";
+    } else if (playerSelection.toUpperCase() == "PAPER" && computerSelection == "ROCK") {
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "ROCK") {
+        return "You lose! Rock beats Scissors!";
+    } else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection == "PAPER") {
+        return "You win! Scissors beats Paper!";
+    } else {
+        return "nothing"
+    }
 
 
 
@@ -17,7 +34,6 @@ function playRound(playerSelection, computerSelection) {
 
 
 
-    
 
     
 }
